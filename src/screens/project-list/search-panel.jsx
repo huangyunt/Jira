6348ec69f841e9220d564c0775131205afcb944c
Huangyunze <973113137@qq.com>
2022-06-17 11:39:11 +0800

    feat: 实现表单

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
 export const SearchPanel = () => {
-    const [first, setfirst] = useState(second);
+    const [param, setParam] = useState({
+        projectName: "",
+        personId: "",
+    });
+    const [users, setUsers] = useState([]);
+    const [list, setList] = useState({});
+    useEffect(() => {
+        fetch("").then(async (res) => {
+            if (res.ok) {
+                setList(await res.json());
+            }
+        });
+        return () => {};
+    }, [param]);
 
-    return <div>SearchPanel</div>;
+    return (
+        <div>
+            <input
+                onChange={(evt) =>
+                    setParam({
+                        ...param,
+                        projectName: evt.target.value,
+                    })
+                }
+            ></input>
+            <select
+                value=""
+                onChange={(evt) =>
+                    setParam({
+                        ...param,
+                        personId: evt.target.value,
+                    })
+                }
+            >
+                <option value="">负责人</option>
+                {users.map((user) => (
+                    <option value={user.id}>{user.name}</option>
+                ))}
+            </select>
+        </div>
+    );
 };
