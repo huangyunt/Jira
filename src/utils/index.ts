85999d1f19eeba4d6497248069b21fcf61c5331f
Huangyunze <973113137@qq.com>
2022-08-02 02:31:30 +0800

    chore: add useArray

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,3 +30,18 @@ export const useDebounce = <V>(deps: V, delay?: number): V => {
   }, [deps, delay]);
   return debouncdValue;
 };
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
