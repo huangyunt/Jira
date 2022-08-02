7425d423f2ab8efac334c52317288e9f017f19e3
Huangyunze <973113137@qq.com>
2022-08-02 19:59:02 +0800

    chore: add middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./App.css";
+import { LoginScreen } from "screens/login";
 import { ProjectListScreen } from "screens/project-list";
 function App() {
-    return (
-        <div className="App">
-            <ProjectListScreen />
-        </div>
-    );
+  return (
+    <div className="App">
+      <LoginScreen />
+      {/* <ProjectListScreen /> */}
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
+import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
-import { ProjectListScreen } from "screens/project-list";
 // import "./index.css";
 // import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
-    <React.StrictMode>
-        <ProjectListScreen />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
