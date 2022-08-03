2167564b2116ad07f2a124e8bfb1032712990c45
Huangyunze <973113137@qq.com>
2022-08-03 19:23:50 +0800

    chore: install devtool

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import App from "App";
 import React from "react";
 import ReactDOM from "react-dom";
+import { loadDevTools } from "jira-dev-tool";
 // import "./index.css";
 // import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
